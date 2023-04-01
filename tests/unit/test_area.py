129d5ea492b1a3fe93ac56350fdf434f9415ef21
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-04-01 14:09:02 -0300

    Add new unit tests for the new functionality

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -1,5 +1,18 @@
+import pytest
 from src.area import calculate_area_square
 
 def test_calculate_area_square():
     assert calculate_area_square(2) == 4
     assert calculate_area_square(2.5) == 6.25
+
+def test_calculate_area_square_negative():
+    assert calculate_area_square(-2) == 4
+    assert calculate_area_square(-2.5) == 6.25
+
+def test_calculate_area_square_string():
+    with pytest.raises(TypeError):
+        calculate_area_square("2")
+
+def test_calculate_area_square_list():
+    with pytest.raises(TypeError):
+        calculate_area_square([2])

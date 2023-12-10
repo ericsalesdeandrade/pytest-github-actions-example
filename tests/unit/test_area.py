89749284510e179bd658f450faab9a3c5937dc23
djiwandou <djiwandou@schoters.com>
2023-12-10 11:13:54 +0700

    chore: Add styling peacock

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -1,6 +1,6 @@
 import pytest
 from src.area import calculate_area_square
-
+#positive case test - add comment to test github actions
 def test_calculate_area_square():
     assert calculate_area_square(2) == 4
     assert calculate_area_square(2.5) == 6.25
